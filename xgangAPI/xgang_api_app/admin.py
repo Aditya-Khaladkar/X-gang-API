94c86b58c0e192c6188f7023ca62e39e50b8ae1b
intel <adityakhaladkar98@gmal.com>
2023-05-28 12:31:41 +0530

    created model and registered it

--- xgangAPI/xgang_api_app/admin.py
+++ xgangAPI/xgang_api_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from xgang_api_app . models import *
 
 # Register your models here.
+admin.site.register(xgang)
--- xgangAPI/xgang_api_app/models.py
+++ xgangAPI/xgang_api_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class xgang(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    choice = (("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5))
+    chutyap = models.CharField(choices=choice, max_length=1)
+    githubLink = models.CharField(max_length=50)

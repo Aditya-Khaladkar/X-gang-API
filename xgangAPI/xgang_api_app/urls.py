0cf8e1d6791f7e4faff591f15859bbaf2fc94340
intel <adityakhaladkar98@gmal.com>
2023-05-28 12:42:29 +0530

    added serializers and created views

--- xgangAPI/xgang_api_app/urls.py
+++ xgangAPI/xgang_api_app/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+from .views import *
+
+from rest_framework import routers
+
+# define the router
+router = routers.DefaultRouter()
+ 
+# define the router path and viewset to be used
+router.register(r'xgang', XgangViewSet)
+ 
+
 urlpatterns = [
-    path('', views.home),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- xgangAPI/xgang_api_app/views.py
+++ xgangAPI/xgang_api_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("this is it")
+# import viewsets
+from rest_framework import viewsets
+
+# import local data
+from .serializers import serializers
+from .models import xgang
+
+# create a viewset
+class XgangViewSet(viewsets.ModelViewSet):
+	# define queryset
+	queryset = xgang.objects.all()
+	
+	# specify serializer to be used
+	serializer_class = serializers

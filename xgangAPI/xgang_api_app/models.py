7094c3fa4c286340d65b6597e849bb2a0f14dcac
intel <adityakhaladkar98@gmal.com>
2023-05-30 11:43:45 +0530

    made githubLink and name unique

--- xgangAPI/xgang_api_app/models.py
+++ xgangAPI/xgang_api_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class xgang(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     choice = (("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5))
     chutyap = models.CharField(choices=choice, max_length=1)
-    githubLink = models.CharField(max_length=50)
+    githubLink = models.CharField(max_length=50, unique=True)
